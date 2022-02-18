d6847684d8d69b83eaa5adfb1d79a0fb348de259
GIGI BERIDZE <82542634+gigi-beridze@users.noreply.github.com>
2022-02-18 20:32:01 +0400

    Update Loading.js

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import './Loading.css'
 export default function Loading(){
-    return <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+    return <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
 }
